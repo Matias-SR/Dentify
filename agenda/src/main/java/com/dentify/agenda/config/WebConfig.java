4deb6788508ec9a6c562ecf44e4a16191b9ce320
Maxipiri <max.caceress@duocuc.cl>
2026-05-25 11:46:35 -0400

    "se agregan mas puertos"

--- agenda/src/main/java/com/dentify/agenda/config/WebConfig.java
+++ agenda/src/main/java/com/dentify/agenda/config/WebConfig.java
@@ -1,21 +1,37 @@
 package com.dentify.agenda.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+// AGENDA (puerto 8083)
+// Consume: Paciente (8081), Dentista (8082), Notificaciones (8085), Prestaciones (8086)
+
 @Configuration
 public class WebConfig {
+
     @Bean
-    public WebClient.Builder webClientBuilder(){
+    public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
-    }
-    @Bean// anotacion para 
-    public WebClient webClientPaciente(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8081/api").build(); // Puerto del Micro de Paciente
     }
 
     @Bean
-    public WebClient webClientMedicos(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8082/api").build(); // Puerto del Micro de Médicos
+    public WebClient webClientPaciente(WebClient.Builder builder) {
+        return builder.baseUrl("http://localhost:8081/api").build();
+    }
+
+    @Bean
+    public WebClient webClientDentista(WebClient.Builder builder) {
+        return builder.baseUrl("http://localhost:8082/api").build();
+    }
+
+    @Bean
+    public WebClient webClientNotificaciones(WebClient.Builder builder) {
+        return builder.baseUrl("http://localhost:8085/api").build();
+    }
+
+    @Bean
+    public WebClient webClientPrestaciones(WebClient.Builder builder) {
+        return builder.baseUrl("http://localhost:8086/api").build();
     }
 }
