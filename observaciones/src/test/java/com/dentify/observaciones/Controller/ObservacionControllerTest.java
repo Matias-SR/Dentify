26164bd274e2cb92f6ce0054c9f2d1614e907ba0
maxipiri-py <max.caceress@duocuc.cl>
2026-06-20 00:31:41 -0400

    PagoTest Listo - Maxi

--- observaciones/src/test/java/com/dentify/observaciones/Controller/ObservacionControllerTest.java
+++ observaciones/src/test/java/com/dentify/observaciones/Controller/ObservacionControllerTest.java
@@ -28,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(ObservacionController.class)
 public class ObservacionControllerTest {
 
